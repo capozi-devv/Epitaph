86b04ffa998c8b2ad7f27b4a0e38648d1c6b43c7
Capozi <capozi.devv@gmail.com>
2025-12-03 18:45:07 -0500

    Initial commit

---
+++ src/main/java/net/capozi/epitaph/Epitaph.java
@@ -0,0 +1,38 @@
+package net.capozi.epitaph;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+
+public class Epitaph implements ModInitializer {
+    public static Map<UUID, JsonObject> publicKeys;
+    public static final Gson GSON = new Gson();
+    public static JsonObject getPlayerMessages(ServerPlayerEntity player) {
+        return publicKeys.get(player.getUuid());
+    }
+    @Override
+    public void onInitialize() {
+        ServerPlayNetworking.registerGlobalReceiver(
+                EpitaphClient.SEND_PLAYER_KEYS,
+                (server, player, handler, buf, responseSender) -> {
+                    String jsonText = buf.readString();
+                    JsonObject json = GSON.fromJson(jsonText, JsonObject.class);
+                    server.execute(() -> {
+                        publicKeys.put(player.getUuid(), json);
+                        System.out.println("Loaded custom messages for " + player.getName().getString());
+                    });
+                }
+        );
+        ServerPlayConnectionEvents.JOIN.register((handler, server, sender) -> {
+            System.out.println("Retrieved message keys for: " + getPlayerMessages(handler.getPlayer()));
+        });
+    }
+}
---
+++ src/main/java/net/capozi/epitaph/mixin/DamageSourceMixin.java
@@ -0,0 +1,22 @@
+package net.capozi.epitaph.mixin;
+
+import com.google.gson.JsonObject;
+import net.capozi.epitaph.Epitaph;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageSource.class)
+public class DamageSourceMixin {
+    @Inject(method = "getDeathMessage", at = @At("HEAD"), cancellable = true)
+    private void customVictimDeathMessage(LivingEntity killed, CallbackInfoReturnable<Text> cir) {
+        if (!(killed instanceof ServerPlayerEntity player)) return;
+        DamageSource source = (DamageSource)(Object)this;
+        JsonObject playerKeys = Epitaph.getPlayerMessages(player);
+    }
+}
